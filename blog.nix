6139823d0f65267cdc949ad04c9edbc3886d7bce
Florian B <yuuki@protonmail.com>
2020-05-04 17:04:08 +0200

    prepare gh pages

--- blog.nix
+++ blog.nix
@@ -26,9 +26,9 @@ let
     '';
 
     installPhase = ''
-      mkdir -p $out/public
-      cp out/* $out/public/
-      cp styles.css $out/public/
+      mkdir -p $out/docs
+      cp out/* $out/docs/
+      cp styles.css $out/docs/
     '';
   };
 
